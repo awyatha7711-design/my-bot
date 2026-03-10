53d34bf42ee7f79d85a275570f187ad69887aaa8
awyatha7711-design <awyatha7711@gmail.com>
2026-03-10 19:52:19 +0630

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 import os
+from telegram.ext import ApplicationBuilder, CommandHandler
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def start(update, context):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Bot is running!")
 
-if__name__ == '__main__':
-    TOKEN = os.environ.get('BOT_TOKEN')
-    application = ApplicationBuilder().token(TOKEN).build()
-    
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
-    
-    application.run_polling()
+if __name__ == '__main__':
+    token = os.environ.get('BOT_TOKEN')
+    if token:
+        application = ApplicationBuilder().token(token).build()
+        application.add_handler(CommandHandler('start', start))
+        application.run_polling()
