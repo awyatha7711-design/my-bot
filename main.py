7a0b42a065bf925362f8886562c3fa6fb4eef8d1
awyatha7711-design <awyatha7711@gmail.com>
2026-03-11 20:03:01 +0630

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask
+from threading import Thread
+from telegram.ext import ApplicationBuilder, CommandHandler
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+async def start(update, context):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=)
+
+if __name__ == '__main__':
+    t = Thread(target=run)
+    t.start()
+    token = os.environ.get('BOT_TOKEN')
+    application = ApplicationBuilder().token(token).build()
+    application.add_handler(CommandHandler('start', start))
+    application.run_polling()
