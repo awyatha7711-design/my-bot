a8915bb50c14272c748592f5f121d10b2c894830
awyatha7711-design <awyatha7711@gmail.com>
2026-03-12 09:09:30 +0630

    Update main.py

--- main.py
+++ main.py
@@ -7,17 +7,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot is running!"
+    return "Server is Live"
 
 def run():
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
 
 async def start(update, context):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Bot is online and working!")
 
 if __name__ == '__main__':
     t = Thread(target=run)
     t.start()
+    
     token = os.environ.get('BOT_TOKEN')
     application = ApplicationBuilder().token(token).build()
     application.add_handler(CommandHandler('start', start))
